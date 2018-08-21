acd073e9ada5d6e438dfeb7f6a073a01271629bc
Jens Reimann <jreimann@redhat.com>
2018-08-21 11:47:52 +0200

    Switch to the lexicon DNS API

--- letsencrypt/create_cert.sh
+++ letsencrypt/create_cert.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+#
+# Create a LE cert with a set of wildcard names.
+#
+# This script makes used of the "lexicon" DNS API. If you cannot use this API,
+# then you need to figure it out on your own. Sorry.
+#
+
 set -e
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
@@ -8,23 +15,21 @@ die() { echo "$*" 1>&2 ; exit 1; }
 
 source "${SCRIPT_DIR}/../config"
 
-export PLESK_URL
-export PLESK_USER
-export PLESK_PASSWORD
-export BASE_DOMAIN
+export PROVIDER
+export LEXICON_OPTS
 
-test -n "$ACME_SH" || die "'ACME_SH' not set"
-test -x "${ACME_SH}/acme.sh" || die "'ACME_SH' doesn't point to a directory containing an executuable 'acme.sh' script"
-
-mkdir -p "$ACME_SH/dnsapi"
-cp "${SCRIPT_DIR}/../plesk-dns/acme_sh_dns_plesk.sh" "$ACME_SH/dnsapi/dns_plesk.sh"
-cp -r "${SCRIPT_DIR}"/../plesk-dns/*.py "$ACME_SH/"
+if [ -z "$ACME_SH" ]; then
+	which acme.sh &>/dev/null && ACME_SH=acme.sh || die "'ACME_SH' not set and 'acme.sh' is not in the PATH"
+else
+	ACME_SH="${ACME_SH}/acme.sh"
+	test -x "${ACME_SH}" || die "'ACME_SH' doesn't point to a directory containing the 'acme.sh' script"
+fi
 
 DOMAINS=
 
-for i in $(seq -w 0 50); do
+for i in $(seq -w 0 25); do
 	DOMAINS="$DOMAINS -d cluster${i}.${SUB_DOMAIN_PART}.${BASE_DOMAIN}"
 	DOMAINS="$DOMAINS -d *.cluster${i}.${SUB_DOMAIN_PART}.${BASE_DOMAIN}"
 done
 
-"$ACME_SH/acme.sh" $ACME_OPTS --issue $DOMAINS --dns dns_plesk
+"$ACME_SH" $ACME_OPTS --issue $DOMAINS --dns dns_lexicon
